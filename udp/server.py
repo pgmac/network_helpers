c912b49965005645b840d044a317490e022c6f23
Paul Macdonnell <pmacdonnell@creditsense.com.au>
2018-09-11 14:43:01 +1000

    Actual server code

--- udp/server.py
+++ udp/server.py
@@ -1,6 +1,23 @@
-echo "# network_helpers" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin git@github.com:pgmac/network_helpers.git
-git push -u origin master
+#!/usr/bin/env python2
+
+import socket
+import sys
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind the socket to the port
+server_address = ('localhost', 10000)
+print >>sys.stderr, 'starting up on %s port %s' % server_address
+sock.bind(server_address)
+
+while True:
+    print >>sys.stderr, '\nwaiting to receive message'
+    data, address = sock.recvfrom(4096)
+
+    print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
+    print >>sys.stderr, data
+
+    if data:
+        sent = sock.sendto(data, address)
+        print >>sys.stderr, 'sent %s bytes back to %s' % (sent, address)
